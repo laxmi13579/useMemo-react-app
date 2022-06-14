3748e41535bd08959a544429a37c37d56847be3a
laxmi13579 <laxmi.yadav370515@gmail.com>
2022-06-14 09:08:21 +0545

    change theme and slow function to compute square number

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState();
+  const [dark, setDark] = useState(false);
+  const squareNum =  useMemo(()=>{
+    return slowFunction(number);
+  },[number])
+
+  const theme = useMemo(()=>{
+    return {
+      background: dark ? 'black' : 'white',
+      color: dark ? 'white' : 'black',
+    }},[dark])
+
+  useEffect(()=>{
+    console.log('theme changed')
+  },[theme])
+
+  const themeHandler = () => {
+    setDark(preDart => !preDart);
+    console.log('dark',dark)
+  }
+
+  function slowFunction(num){
+    for(let i = 0; i<=1000000000;i++){}
+    return num**2;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type='number' value={number} onChange={(e)=>setNumber(parseInt(e.target.value))}/>
+      <button style={theme} onClick={themeHandler}>change theme</button>
+      <p>square of {number} is {squareNum}</p>
+    </>
   );
 }
 
